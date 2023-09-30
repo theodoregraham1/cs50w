3c592b10f835eac253b19f4962519f0089d64f77
theodoregraham1 <117173439+theodoregraham1@users.noreply.github.com>
2023-09-30 17:31:49 +0100

    Finish part 2

---
+++ commerce/auctions/forms.py
@@ -0,0 +1,24 @@
+from django.forms import ModelForm, Textarea, URLInput, TextInput, NumberInput, URLField, CharField
+
+from .models import User, Listing, Bid, Comment
+
+class AddListingForm(ModelForm):
+    image_url = URLField(required=False, 
+                         widget=URLInput(attrs={"class": "form-control", 
+                                                "placeholder": "Image",},))
+    category = CharField(required=False, 
+                         widget=TextInput(attrs={"class": "form-control", 
+                                         "placeholder": "Category",}))
+
+    class Meta:
+        model = Listing
+        fields = ["title", "description", "starting_bid", "image_url", "category", "user_id"]
+
+        widgets = {
+            "title": TextInput(attrs={"class": "form-control", 
+                                      "placeholder": "Title",}),
+            "description": Textarea(attrs={"class": "form-control", 
+                                           "placeholder": "Description",}),
+            "starting_bid": NumberInput(attrs={"class": "form-control", 
+                                               "placeholder": "Starting Bid",}),
+        }
